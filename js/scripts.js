0f2517e7f05ebf4f385161b2c2c8e76e2470f67e
Amber-DiNenna <amberdinenna@gmail.com>
2022-06-17 15:04:03 -0700

    WIP: can't assign values to radio name input values.

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,19 @@ window.onload = function(){
   document.querySelector("form").onsubmit = function(event) {
     hideResults();
     event.preventDefault();
+
+  const endValue = document.querySelector("input[name=end]:checked").value;
+  const eyeValue = document.querySelector('input[name="eye"]:checked').value;
+  const fluffValue = document.querySelector('input[name="fluff"]:checked').value;
+  const functionValue = document.querySelector('input[name="function"]:checked').value;
+  const toonValue = document.querySelector('input[name="toon"]:checked').value;
+  const viewValue = document.querySelector('input[name="view"]:checked').value;
+  const dudeValue = document.querySelector('input[name="dude"]:checked').value;
+  const peopleValue = document.querySelector('input[name="people"]:checked');
+  const ocdValue = document.querySelector('input[name="ocd"]:checked').value;
+
+  if (endValue === "option2" && eyeValue === "option2" && functionValue === "option2" && viewValue === "option1" && peopleValue === "option2") {
+    document.getElementById("python").style.display = "block;";
+  }
   }
 }
