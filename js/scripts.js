aed3a96e2e70ee1e77f03add2fa2adf31608ce17
Amber-DiNenna <amberdinenna@gmail.com>
2022-06-17 16:05:03 -0700

    Add onreset function to clear results and input values.

--- js/scripts.js
+++ js/scripts.js
@@ -14,18 +14,25 @@ window.onload = function(){
     hideResults();
     event.preventDefault();
 
-  const endValue = document.querySelector("input[name=end]:checked").value;
-  const eyeValue = document.querySelector('input[name="eye"]:checked').value;
-  const fluffValue = document.querySelector('input[name="fluff"]:checked').value;
-  const functionValue = document.querySelector('input[name="function"]:checked').value;
-  const toonValue = document.querySelector('input[name="toon"]:checked').value;
-  const viewValue = document.querySelector('input[name="view"]:checked').value;
-  const dudeValue = document.querySelector('input[name="dude"]:checked').value;
-  const peopleValue = document.querySelector('input[name="people"]:checked');
-  const ocdValue = document.querySelector('input[name="ocd"]:checked').value;
+  let endValue = document.querySelector("input[name=end]:checked").value;
+  let eyeValue = document.querySelector("input[name=eye]:checked").value;
+  let fluffValue = document.querySelector("input[name=fluff]:checked").value;
+  let whatValue = document.querySelector("input[name=what]:checked").value;
+  let toonValue = document.querySelector("input[name=toon]:checked").value;
+  let viewValue = document.querySelector("input[name=view]:checked").value;
+  let dudeValue = document.querySelector("input[name=dude]:checked").value;
+  let peopleValue = document.querySelector("input[name=people]:checked");
+  let ocdValue = document.querySelector("input[name=ocd]:checked").value;
 
   if (endValue === "option2" && eyeValue === "option2" && functionValue === "option2" && viewValue === "option1" && peopleValue === "option2") {
     document.getElementById("python").style.display = "block;";
-  }
-  }
+
+  } else if (endValue === "option1" && eyeValue === "option1" && functionValue === "option1" && viewValue === "option2" && peopleValue === "option1") {
+    document.getElementById("javascript").style.display = "block;";
+    }
+  };
+};
+
+window.onreset = function() {
+  hideResults();
 }
