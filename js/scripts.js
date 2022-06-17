5597778245985a83177f7f00676f2226c4f38955
Amber-DiNenna <amberdinenna@gmail.com>
2022-06-17 12:44:13 -0700

    Add hideResults function to declare result ids to be hidden.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// User Interface Logic
+
+function hideResults() {
+  document.getElementById("python").style.display = "none";
+  document.getElementById("javascript").style.display = "none";
+  document.getElementById("ruby").style.display = "none";
+}
